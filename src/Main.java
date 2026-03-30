871244623b1e378018da52179441bc8ac190bca3
gabriel <gabrielg.profissional04@gmail.com>
2026-03-30 15:33:05 -0300

    Main atualziada, v1 do projeto funcionando.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Gerenciador de Treinos ===");
+        System.out.print("Qual é a divisão de treino de hoje?: ");
+        String nomeTreino = scanner.nextLine();
+
+        //Criando a ficha com o nome digitado pelo usuário
+        Treino treinoDeHoje = new Treino(nomeTreino);
+
+        String continuar = "S"; //Variável de controle do loop
+
+        //O loop irá continuar rodando enquanto o usuário digitar "S" ou "s"
+        while (continuar.equalsIgnoreCase("S")){
+            System.out.println("\n--- Adicionando novo exercício ---");
+
+            System.out.print("Nome do exercício: ");
+            String nomeExercicio = scanner.nextLine();
+
+            System.out.print("Número de séries: ");
+            int series = scanner.nextInt();
+
+            System.out.print("Número de repetções: ");
+            int repeticoes = scanner.nextInt();
+
+            System.out.print("Carga atual (kg): ");
+            double carga = scanner.nextDouble();
+
+            scanner.nextLine();
+
+            Exercicio novoExercicio = new Exercicio(nomeExercicio, series, repeticoes, carga);
+            treinoDeHoje.adicionarExercicio(novoExercicio);
+
+            System.out.print("\nDeseja adicionar outro exercício nesta ficha? (S/N): ");
+            continuar = scanner.nextLine();
+
+        }
+
+        System.out.println("\n Pronto! Aqui está o resumo do seu treino cadastrado: ");
+        treinoDeHoje.mostrarFicha();
+
+        scanner.close();
     }
 }
